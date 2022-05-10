5a827fd3865fadeafd8494839bbb0263ac80dfc7
Deon Jordaan <hello@deon.dev>
2022-05-10 21:58:33 +0200

    Trying a custom hook to fetch data from Airtable.

--- src/store/project-slice.tsx
+++ src/store/project-slice.tsx
@@ -1,19 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import Class from '../types/class';
 import Student from '../types/student';
+import FetchAirtable from './FetchAirtable';
 
 // The ID of this base is app8ZbcPx7dkpOnP0
 
 interface ProjectState {
 	isLoggedIn: boolean;
-	currentStudent: Student | undefined;
+	currentStudent: Student[];
 	classes: Class[];
 }
 
 const initialProjectState: ProjectState = {
 	isLoggedIn: false,
-	currentStudent: undefined,
+	currentStudent: [],
 	classes: [],
 };
 
@@ -24,46 +25,43 @@ const projectSlice = createSlice({
 		IS_LOGGED_IN(state) {
 			state.isLoggedIn = !state.isLoggedIn;
 		},
-		SET_CURRENT_STUDENT(state, action) {},
+		SET_CURRENT_STUDENT(state, action: PayloadAction<Student[]>) {
+			state.currentStudent = action.payload;
+		},
 	},
 });
 
 export const fetchStudentData = (student: string) => {
 	return async (dispatch: any) => {
-		const getStudentData = async () => {
-			const Airtable = require('airtable');
+		const { sendRequest: fetchStudent } = FetchAirtable();
+		// const { isLoading, error, sendRequest: fetchStudent } = useAirtable();
 
-			const base = new Airtable({
-				apiKey: process.env.REACT_APP_AIRTABLE_API_KEY,
-			}).base(process.env.REACT_APP_AIRTABLE_BASE_ID);
-
-			const studentName = student;
-
-			console.log(typeof studentName);
-
-			const studentData = await base('Students')
-				.select({ filterByFormula: `FIND(Name, '${studentName}')` })
-				.firstPage();
-
-			console.log(studentData);
-
-			const extractStudentData = studentData.map((data: any) => {
+		const getStudentData = (studentData: any[]) => {
+			const extractedStudentData = studentData.map((data) => {
 				return {
 					name: data.fields.Name,
 					classes: data.fields.Classes,
 				};
 			});
 
-			return extractStudentData;
+			// return extractStudentData;
+			dispatch(
+				projectSlice.actions.SET_CURRENT_STUDENT(extractedStudentData)
+			);
 		};
 
-		try {
-			const student = await getStudentData();
-			dispatch(projectSlice.actions.SET_CURRENT_STUDENT(student));
-			console.log(student);
-		} catch (error) {
-			console.log(error);
-		}
+		// const currentStudent = await getStudentData();
+
+		fetchStudent(
+			{
+				apiKey: process.env.REACT_APP_AIRTABLE_API_KEY,
+				baseId: process.env.REACT_APP_AIRTABLE_BASE_ID,
+				queryData: student,
+				queryField: 'Name',
+				baseName: 'Students',
+			},
+			getStudentData
+		);
 	};
 };
 
