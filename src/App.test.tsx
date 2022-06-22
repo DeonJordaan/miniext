c9cc74403609235ce01461c96e4bab49d8c028ca
Deon Jordaan <hello@deon.dev>
2022-06-22 14:01:27 +0200

    Trying to test rendering of ClassDetail component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,21 @@
 import { render, screen, waitFor } from './test-utils';
-import { fireEvent } from './test-utils';
+// import { fireEvent } from './test-utils';
 
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-describe('Expand body button', async () => {
-    it('should render ClassDetail when Login is clicked', () => {
-        // render your component
-        render(<App />) 
-        // access your button
-        const loginButton = screen.getByText('Login') 
-        // simulate button click
-        fireEvent.click(loginButton); 
+describe('Render ClassDetail component', () => {
+	it('should render ClassDetail when Login is clicked', async () => {
+		// render your component
+		render(<App />);
+		// access your button
+		const loginButton = screen.getByText('Login');
+		// simulate button click
+		userEvent.click(loginButton);
 
-        // expect result
-        await waitFor(() =>  
-            expect(screen.getByText("Some content")).toBeInTheDocument()
-        
-       });
-})
+		// expect result
+		// await waitFor(() =>
+		await screen.not.findByText('Student Name:');
+	});
+	// }
+});
